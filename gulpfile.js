028cc748a32bd3edea62f006676b92448a564c98
sergey <dimadeliev@gmail.com>
2022-09-05 09:02:58 +0300

    5

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const { src, dest }  = require('gulp');
+const { src, dest } = require('gulp');
 
-const scss = require('gulp-sass');
+const scss = require('gulp-sass')(require('sass'));
 
 function styles() {
-	return src ('app/scss/style.scss')
-	.pipe(scss())
-	.pipe(dest('app/css'))
+   return src('app/scss/style.scss')
+      .pipe(scss())
+      .pipe(dest('app/css'))
 }
 
-exports.styles = 	styles;
+exports.styles = styles;
