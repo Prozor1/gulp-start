22927ac783507501d15dc6058a83a45a69115379
Prozor1 <sergeygrygorashenko@gmail.com>
2022-09-07 00:15:11 +0300

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,54 @@
 const { src, dest, watch, parallel } = require('gulp');
 
-const scss   = require('gulp-sass')(require('sass'));
+const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
 
 function browsersync() {
-   browserSync.init({
-      server: {
-         baseDir: 'app/'
-      }
-   })
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		}
+	})
 }
 
+function scripts() {
+	return src([
+		'node_modules/jquery/dist/jquery.js',
+		'app/js/main.js'
+	])
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+
+
 function styles() {
-   return src('app/scss/style.scss')
-      .pipe(scss())
-      .pipe(scss({outputStyle: 'compressed'}))
-      .pipe(concat('style.min.css'))
-      .pipe(dest('app/css'))
-      .pipe(browserSync.stream())
+	return src('app/scss/style.scss')
+		.pipe(scss())
+		.pipe(scss({ outputStyle: 'compressed' }))
+		.pipe(concat('style.min.css'))
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 10 version'],
+			grid: true
+		}))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
 }
 
 function watching() {
-   watch(['app/scss/**/*.scss'], styles);
-   watch("app/*.html").on('change', browserSync.reload);
+	watch(['app/scss/**/*.scss'], styles);
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+	watch("app/*.html").on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
+exports.scripts = scripts;
 
-exports.default = parallel(browsersync , watching);
+exports.default = parallel(scripts, browsersync, watching);
